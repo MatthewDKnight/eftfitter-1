104907666f523c8aa2fa8765a0c5be5b05fee6e4
Jonathan Davies <jd2616@lx00.hep.ph.ic.ac.uk>
2019-10-15 14:49:29 +0100

    First steps towards getting this working

--- read.py
+++ read.py
@@ -3,5 +3,28 @@ with open("equations/stage0_xs.txt", "r") as file:
 	for row in file:
 		rows.append(row)
 
-print(contents)
+import numpy
+import parameters_config_EFT
+from parameters_config_EFT import PARAMS
+A_matrix=numpy.zeros(shape=(len(PARAMS), len(PARAMS)))
+B_matrix=numpy.zeros(shape=(len(PARAMS), len(PARAMS), len(PARAMS)))
+
+EFT_param_list=[]
+EFT_param_names=[]
+
+for i in PARAMS.keys():
+	EFT_param_list.append(i.split('_'))
+	EFT_param_names.append(i.split('_')[0])
+
+
+scaling_functions_decomposed=[]
+for j in rows:
+	scaling_functions_decomposed.append(j.split('+','-'))
+
+
+
+
+	
+
+
 	
