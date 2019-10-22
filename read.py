17e4e9fa245e0d0eb0efc8f26a7c6bd348840811
Matthew Knight <mdk16@lx00.hep.ph.ic.ac.uk>
2019-10-22 14:29:53 +0100

    Tidying read.py

--- read.py
+++ read.py
@@ -23,41 +23,57 @@ def splitRow(string):
 	return pieces
 
 def decomposeTerm(term):
-	#find where the first term starts by finding lowercase letter
+	"""
+	Takes in a term like like '-0.682 * cT ' or '+ 10.8796 * cWW * cWW '
+	and seperates it into the coefficient and the corresponding EFT
+	parameter(s).
+	"""
 	term = term.replace("*", "") #remove *
 
+	#find where the first parameter by finding first lowercase letter in string
 	for i in range(len(term)):
 		if ord(term[i])>97 and ord(term[i])<122: #use ascii
 			first_half = term[:i] #contains coefficient
 			second_half = term[i:] #contains term(s)
 			break
-	first_half = first_half.replace(" ", "") #remove spaces
+
+	#for first_half, remove spaces and then float to give coefficient
+	first_half = first_half.replace(" ", "")
 	coefficient = float(first_half) 
 	
-	terms = second_half.split(" ", 1)
-	
-	if terms[1] == "": #if only one term
-		del terms[1]
+	#make list of EFT parameters in term
+	params = second_half.split(" ", 1)
+	if params[1] == "": #if only one term
+		del params[1]
 	else:
-		terms[1] = terms[1].replace(" ", "")
-	return coefficient, terms 
+		params[1] = params[1].replace(" ", "")
+	return coefficient, params
 
 
 def new_get_x(vals=np.zeros(shape=(39, 2)).tolist(), MINDEX=0):
+	#read in text file, row by r
 	with open("equations/stage1_1_xs.txt", "r") as file:
 		rows=[]
 		for row in file:
 			rows.append(row)
+
+	#initialise empty A and B arrays
 	A_matrix=np.zeros(shape=(len(PARAMS), len(rows)))
 	B_matrix=np.zeros(shape=(len(PARAMS), len(PARAMS), len(rows)))
+       
         #EFT_param_vector=np.array([vals[i][1] for i in range(len(vals))])
 
 	EFT_param_list=[]
 	EFT_param_names=[]
-
+	
+	#create a list of the EFT parameter names, EFT_param_names
 	for i in PARAMS.keys():
 		EFT_param_list.append(i.split('_'))
 		EFT_param_names.append(i.split('_')[0])
+
+	"""
+	
+	"""
 	index1= EFT_param_names.index('cWWMinuscB')
 	index2= EFT_param_names.index('cWWPluscB')
 
@@ -68,74 +84,17 @@ def new_get_x(vals=np.zeros(shape=(39, 2)).tolist(), MINDEX=0):
         EFT_param_names[index1]='cWW'
         EFT_param_names[index2]='cB'
 
-	print(EFT_param_names)
-	
-	"""
-	#print (EFT_param_names)
-	scaling_functions_decomposed=[]
-	for j in rows:
-		row_decomposed=[]
-		totally_split_row=list(j)
-		unsplit_terms=[]
-		for k in range(len(totally_split_row)):
-			if totally_split_row[k]=='+':
-				row_decomposed.append("".join(unsplit_terms))
-				unsplit_terms=['+']
-               		elif totally_split_row[k]=='-':
-				row_decomposed.append("".join(unsplit_terms))
-				unsplit_terms=['-']
-			else:
-				unsplit_terms.append(totally_split_row[k])
-		scaling_functions_decomposed.append(row_decomposed)
-	"""
+	#print(EFT_param_names)	
+
 	scaling_functions_decomposed = []
 	for j in rows:
 		split_row = splitRow(j)
 		del split_row[0] #removes ggH:1 bit
 		scaling_functions_decomposed.append(split_row)
-		print(j)
 		print(split_row)
-
-	"""	
-	for y in range(len(scaling_functions_decomposed)):
-		for term in scaling_functions_decomposed[y]:
-			print('term; '+term)
-			for z in range(len(EFT_param_names)):
-				if ' '+EFT_param_names[z]+' ' in term:
-					#print('EFT_param; '+EFT_param_names[z])
-					if term.count('*') == 1:
-						#print('EFT_param; '+EFT_param_names[z])
-					#if not any(x in term.replace(EFT_param_names[z], '', 1) for x in EFT_param_names):
-						if '+ ' in term:
-							#print('1Dnew_entry; '+term.replace(' * ' +EFT_param_names[z], '', 1).replace('+ ', '', 1))
-							new_entry=float(term.replace( ' * '+EFT_param_names[z], '', 1).replace('+ ', '', 1))
-						elif '- ' in term:
-							#print('1Dnew_entry; '+term.replace(' * '+EFT_param_names[z], '', 1).replace('- ', '', 1))
-							new_entry=-float(term.replace(' * '+EFT_param_names[z], '', 1).replace('- ', '', 1))
-						else:
-							#print('1Dnew_entry; '+term.replace(' * '+EFT_param_names[z], '', 1))
-							new_entry=float(term.replace(' * '+EFT_param_names[z], '', 1)) 
-						A_matrix[z][y]=new_entry
-					elif term.count('*') == 2:
-						EFT_param_1=EFT_param_names[z]
-						#print('EFT_param_1; '+EFT_param_1)
-						term=term.replace(' * '+EFT_param_names[z], '', 1)
-						for a in range(len(EFT_param_names)):
-							if ' '+EFT_param_names[a]+' ' in term:
-								EFT_param_2=EFT_param_names[a]
-								#print('EFT_param_2; '+EFT_param_2)
-								if '+ ' in term:
-									#print('2Dnew_entry; '+term.replace(' * '+EFT_param_2, '', 1).replace('+ ', ''))
-									new_entry=float(term.replace(' * '+EFT_param_2, '', 1).replace('+ ', ''))
-								elif '- ' in term:
-									#print('2Dnew_entry; '+term.replace(' * '+EFT_param_2, '', 1).replace('- ', ''))
-									new_entry=-float(term.replace(' * '+EFT_param_2, '', 1).replace('- ', ''))
-								else:
-									#print('2Dnew_entry; '+term.replace(' * '+EFT_param_2, '', 1))
-									new_entry=float(term.replace(' * '+EFT_param_2, '', 1))
-								B_matrix[z][a][y]=new_entry
-	"""					
 	
+	#print(scaling_functions_decomposed)
+
 	for y in range(len(scaling_functions_decomposed)): #for each bin
 		for term in scaling_functions_decomposed[y]: #for each term in equation for this bin
 			#print(term)
